879eeaf1e6260e8266f0ee85f58d092cfcf0f9e4
Kimmo Polojarvi <kimmo.polojarvi@gmail.com>
2019-06-17 19:04:55 +0300

    add person function added

--- mongo.js
+++ mongo.js
@@ -1,32 +1,32 @@
 const mongoose = require('mongoose')
 
 if ( process.argv.length<3 ) {
-  console.log('give password as argument')
+  console.log('give password as argument, then name & number')
   process.exit(1)
 }
 
 const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
 
 const url =
   `mongodb+srv://Yomyssy:${password}@cluster0-j09qz.mongodb.net/test?retryWrites=true&w=majority`
 
 mongoose.connect(url, { useNewUrlParser: true })
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Person = mongoose.model('Person', personSchema)
 
-const note = new Note({
-  content: 'HTML is Easy',
-  date: new Date(),
-  important: true,
+const person = new Person({
+  name: name,
+  number: number
 })
 
-note.save().then(response => {
-  console.log('note saved!');
+person.save().then(response => {
+  console.log('Contact saved!');
   mongoose.connection.close();
 })
