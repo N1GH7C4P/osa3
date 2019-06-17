e4e49739132774ff3d0ffcb07873ea89d343b7dc
Kimmo Polojarvi <kimmo.polojarvi@gmail.com>
2019-06-17 19:38:57 +0300

    added list function

--- mongo.js
+++ mongo.js
@@ -5,37 +5,38 @@ if ( process.argv.length<3 ) {
   process.exit(1)
 }
 
-const password = process.argv[2]
-const name = process.argv[3]
-const number = process.argv[4]
-
 const url =
   `mongodb+srv://Yomyssy:${password}@cluster0-j09qz.mongodb.net/test?retryWrites=true&w=majority`
 
 mongoose.connect(url, { useNewUrlParser: true })
 
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-})
-
-const Person = mongoose.model('Person', personSchema)
-
-const person = new Person({
-  name: name,
-  number: number
-})
-
-person.save().then(response => {
-  console.log('Contact saved!');
-  mongoose.connection.close();
-})
-
-mongoose.connect(url, { useNewUrlParser: true })
-
-Person.find({}).then(result => {
+if(process.argv.length > 3){
+  const password = process.argv[2]
+  const name = process.argv[3]
+  const number = process.argv[4]
+  const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+  })
+  const Person = mongoose.model('Person', personSchema)
+  const person = new Person({
+    name: name,
+    number: number
+  })
+  person.save().then(response => {
+    console.log('Contact saved!');
+    mongoose.connection.close();
+  })
+}else{
+  Person.find({}).then(result => {
   result.forEach(person => {
     console.log(person)
   })
   mongoose.connection.close()
 })
+}
+
+
+
+
+
