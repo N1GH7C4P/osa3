f078230e6bd3ef4653b2df318c0efb3cfc73c908
Kimmo Polojarvi <kimmo.polojarvi@gmail.com>
2019-06-18 13:25:18 +0300

    added inspect singular person functionality

--- index.js
+++ index.js
@@ -12,80 +12,53 @@ app.use(morgan('tiny'))
 app.use(cors())
 app.use(express.static('build'))
 
-let now = new Date()
-let persons = 
-  [
-    { 
-      "name": "Arto Hellas", 
-      "number": "040-123456",
-      "id": 1
-    },
-    { 
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523",
-      "id": 4
-    },
-    { 
-      "name": "Dan Abramov", 
-      "number": "12-43-234345",
-      "id": 3
-    },
-    { 
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122",
-      "id": 4
-    }
-  ]
-
-  app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-      response.json(person.toJSON())
-    })
+app.get('/api/persons/:id', (request, response) => {
+  Person.findById(request.params.id).then(person => {
+    response.json(person.toJSON())
   })
+})
 
-  app.get('/api/info', (req, res) => {
-    res.send(`Phonebook has info for ${persons.length} people.<br></br>${now}`)
+app.get('/api/info', (req, res) => {
+  res.send(`Phonebook has info for ${persons.length} people.<br></br>${now}`)
+})
 
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons.map(person => person.toJSON()))
   })
+})
 
-  app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-      response.json(persons.map(person => person.toJSON()))
-    })
-  })
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
+})
 
-  app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
-  })
+const generateId = () => {
+  const max = 1000000;
+  const randomId = Math.floor(Math.random() * Math.floor(max));
+  return randomId
+}
 
-  const generateId = () => {
-    const max = 1000000;
-    const randomId = Math.floor(Math.random() * Math.floor(max));
-    return randomId
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (body.name === undefined || body.number === undefined) {
+    return response.status(400).json({ error: 'Name or number missing!' })
   }
-  
-  app.post('/api/persons', (request, response) => {
-    const body = request.body
 
-    if (body.name === undefined || body.number === undefined) {
-      return response.status(400).json({ error: 'Name or number missing!' })
-    }
-  
-    const person = new Person ({
-      name: body.name,
-      number: body.number,
-      id: generateId(),
-    })
-  
-    person.save().then(savedPerson => {
-      response.json(savedPerson.toJSON())
-    })
+  const person = new Person ({
+    name: body.name,
+    number: body.number,
+    id: generateId(),
   })
 
-  const PORT = process.env.PORT
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  person.save().then(savedPerson => {
+    response.json(savedPerson.toJSON())
   })
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
--- models/person.js
+++ models/person.js
@@ -25,13 +25,4 @@ personSchema.set('toJSON', {
     }
   })
   
-
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
 module.exports = mongoose.model('NotePerson', personSchema)
