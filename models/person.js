c1b0024021fbd99fda5e61a67a1df2ea211a1e3f
Kimmo Polojarvi <kimmo.polojarvi@gmail.com>
2019-06-19 14:12:42 +0300

    try to fix dotenv

--- models/person.js
+++ models/person.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const url = process.env.MONGODB_URI
+const url = 'mongodb+srv://Yomyssy:sekred@cluster0-j09qz.mongodb.net/test?retryWrites=true&w=majority'
 console.log('connecting to', url)
 
 mongoose.connect(url, { useNewUrlParser: true })
